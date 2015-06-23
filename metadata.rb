57e530564b7f9eeb2dc9a53cb7eef3c248386c78
Franklin Webber <franklin.webber@gmail.com>
2015-06-23 01:29:45 -0700

    Added release mechanism

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name              'chef-fundamentals-windows'
+maintainer        'Chef Software, Inc.'
+maintainer_email  'training@chef.io'
+license           'Creative Commons Attribution-ShareALike 4.0 International (CC BY-SA 4.0)'
+description       'Chef Fundamentals - Windows'
+version           '2.1.1'
+
+asset             "Chef-Fundamentals-Windows.key", "Presentation (Keynote)"
+asset             "Chef-Fundamentals-Windows-keynote_09.key", "Presentation (Keynote 09)"
+asset             "Chef-Fundamentals-Windows.pdf", "Presentation (PDF)"
+asset             "Chef-Fundamentals-Windows-presenter_notes.pdf", "Presentation with Presenter Notes (PDF)"
+asset             "Chef-Fundamentals-Windows-StudentHandouts.docx", "Attendee Handouts (Word)"
+asset             "Chef-Fundamentals-Windows-StudentHandouts.pdf", "Attendee Handouts (PDF)"
+
+local_release_folder "~/Google\\ Drive/Training/Chef\\ Fundamentals\\ -\\ Windows/Releases"
